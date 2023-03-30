63179678088836fcea34725792cf1c0e000248c3
kumar026 <velugulakumar786@gmail.com>
2023-03-30 10:02:15 +0530

    2nd attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,17 +3,17 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    //System.out.println(dp.getPrice());
+    System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
-    dp.addExtraToppings();
+    //dp.addExtraToppings();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(false);
+    Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
-    //p.addTakeaway();
+    p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
